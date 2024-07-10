19ee73264f8edbccfa2c2f76c4610a6ae7d749b8
Shreyagupta05 <140239852+Shreyagupta05@users.noreply.github.com>
2024-07-10 17:30:50 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";//treat all JS codeas newer version
+
+//alert(3+3)// we are useing node js,not browser
+
+/*console.log(3+3) ;
+console.log("shee")*/
+ //code readability should be high
+
+
+
+let name="shreya"
+let age=19
+let isLoggedIn=true
+let state=null
+
+//number datatype=>2to power 53
+//bigint
+//string datatype
+//boolean=> true/false
+//null ===it is standalone value
+
+//undefined=> now time value not assign
+
+//symbol=>unique ki leya use kate hai 
+
+//object
+
+
+
+console.log(typeof "shreya");
+console.log(typeof null);// output is object type
+console.log(typeof undefined); //output is undefined
+
+
+
